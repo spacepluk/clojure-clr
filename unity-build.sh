f773b42b55791fc96550dccff437cc0d428a1426
Ramsey Nasser <ram@nas.sr>
2014-04-14 17:10:17 -0400

    Update unity-build to use stock mono xbuild

--- unity-build.sh
+++ unity-build.sh
@@ -1,1 +1,1 @@
-EnableNuGetPackageRestore=true  /Applications/Unity/Unity.app/Contents/Frameworks/MonoBleedingEdge/bin/mono /Applications/Unity/Unity.app/Contents/Frameworks/MonoBleedingEdge/lib/mono/2.0/xbuild.exe Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
+EnableNuGetPackageRestore=true xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
