5e44e765c63a0c4e477bb9416b12af8d93d75554
Ramsey Nasser <ram@nas.sr>
2014-04-13 20:06:26 -0400

    Add terminal flag to fix broken build

--- unity-build.sh
+++ unity-build.sh
@@ -1,1 +1,1 @@
-/Applications/Unity/Unity.app/Contents/Frameworks/MonoBleedingEdge/bin/mono /Applications/Unity/Unity.app/Contents/Frameworks/MonoBleedingEdge/lib/mono/2.0/xbuild.exe Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
+EnableNuGetPackageRestore=true  /Applications/Unity/Unity.app/Contents/Frameworks/MonoBleedingEdge/bin/mono /Applications/Unity/Unity.app/Contents/Frameworks/MonoBleedingEdge/lib/mono/2.0/xbuild.exe Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
