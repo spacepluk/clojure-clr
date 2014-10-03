c57351d8abbb006a08cf2d773ff36e45d0e81cfa
Ramsey Nasser <ram@nas.sr>
2014-10-03 13:00:26 -0400

    Compile Release instead of Debug + clean before build

--- unity-build.sh
+++ unity-build.sh
@@ -1,1 +1,2 @@
-EnableNuGetPackageRestore=true xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
+rm -fr dist bin
+EnableNuGetPackageRestore=true xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Release 3.5" /p:Platform="Any CPU"
