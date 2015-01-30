77386bd4f3657970cf10b0547c72ff7db5cc2245
Ramsey Nasser <ram@nas.sr>
2015-01-29 23:28:11 -0500

    Update unity-install to new layout

--- unity-install.sh
+++ unity-install.sh
@@ -1,16 +1,17 @@
 echo "clean up old dlls"
-rm -fr $1/Assets/Arcadia/*.dll
+rm -fr $1/Assets/Arcadia/Infrastructure/*.dll
+rm -fr $1/Assets/Arcadia/Compiled/*.dll
 
 echo "copy infrastructure"
-cp bin/3.5/Release/{Clojure,Microsoft.{Dynamic,Scripting,Scripting.{Core,Metadata}}}.dll $1/Assets/Arcadia/
+cp bin/3.5/Release/{Clojure,Microsoft.{Dynamic,Scripting,Scripting.{Core,Metadata}}}.dll $1/Assets/Arcadia/Infrastructure
 
 echo "copy AOT'd clojure files"
-cp bin/3.5/Release/*.clj.dll $1/Assets/Arcadia/
+cp bin/3.5/Release/*.clj.dll $1/Assets/Arcadia/Compiled
 
 echo "copy clojure source"
-rm -fr $1/Assets/Arcadia/clojure
-cp -r bin/3.5/Release/clojure $1/Assets/Arcadia
-rm -fr $1/Assets/Arcadia/clojure/*samples*
-rm -fr $1/Assets/Arcadia/clojure/*run_tests*
-rm -fr $1/Assets/Arcadia/clojure/*dm-test*
-rm -fr $1/Assets/Arcadia/clojure/*test_*
+rm -fr $1/Assets/Arcadia/Source/clojure
+cp -r bin/3.5/Release/clojure $1/Assets/Arcadia/Source
+rm -fr $1/Assets/Arcadia/Source/clojure/*samples*
+rm -fr $1/Assets/Arcadia/Source/clojure/*run_tests*
+rm -fr $1/Assets/Arcadia/Source/clojure/*dm-test*
+rm -fr $1/Assets/Arcadia/Source/clojure/*test_*
