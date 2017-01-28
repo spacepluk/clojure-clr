c993c976181a219392aed4186b828448495d4bc2
Ramsey Nasser <ram@nas.sr>
2017-01-28 01:59:26 -0500

    Update build and install scripts

--- unity-build.sh
+++ unity-build.sh
@@ -1,2 +1,2 @@
 rm -fr dist bin
-RestorePackages=false xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
+RestorePackages=false xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Release 3.5" /p:Platform="Any CPU"
--- unity-install.sh
+++ unity-install.sh
@@ -1,17 +1,10 @@
-echo "clean up old dlls"
-rm -fr $1/Infrastructure/*.dll
-rm -fr $1/Compiled/*.dll
-
 echo "copy infrastructure"
 cp bin/3.5/Release/{Clojure,Microsoft.{Dynamic,Scripting,Scripting.{Core,Metadata}}}.dll $1/Infrastructure
-
-echo "copy AOT'd clojure files"
-cp bin/3.5/Release/*.clj.dll $1/Compiled
 
 echo "copy clojure source"
 rm -fr $1/Source/clojure
 cp -r bin/3.5/Release/clojure $1/Source
 rm -fr $1/Source/clojure/*samples*
-rm -fr $1/Source/clojure/*run_tests*
+rm -fr $1/Source/clojure/*run_test*
 rm -fr $1/Source/clojure/*dm-test*
 rm -fr $1/Source/clojure/*test_*
