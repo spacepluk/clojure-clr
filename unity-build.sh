cc87e9c758a1a078a316277c2c4dd9ab945cae33
Ramsey Nasser <ram@nas.sr>
2017-01-28 00:05:50 -0500

    Update build script to disable package restore

--- unity-build.sh
+++ unity-build.sh
@@ -1,2 +1,2 @@
 rm -fr dist bin
-EnableNuGetPackageRestore=true xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
+RestorePackages=false xbuild Clojure/build.proj /target:"Dist" /p:Runtime="Mono" /p:Configuration="Debug 3.5" /p:Platform="Any CPU"
